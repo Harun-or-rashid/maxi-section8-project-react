8e39aea0e20f9e887f5212426058a0f465756281
Harun-or-rashid <harun35-2093@diu.edu.bd>
2022-06-17 03:15:15 +0600

    user form added

--- src/components/User/AddUser.js
+++ src/components/User/AddUser.js
@@ -1,5 +1,21 @@
 import React from "react";
 
 const AddUser=(props)=>{
-      
+    const submitHandler=(event)=>{
+        event.preventDefault();
+    };
+
+      return(
+      <div>
+     <form onSubmit={submitHandler}>
+     <label for="username">User Name</label>
+        <input id="username" type="text"/>
+
+        <label for="age">Age</label>
+        <input id="age" type="number"/>
+
+        <button type="submit">Add User</button>
+     </form>
+      </div>)
 };
+export default AddUser;
