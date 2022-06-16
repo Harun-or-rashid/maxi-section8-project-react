4d7cb3ae0daa2554379cc93abe9a8094851f847e
Harun-or-rashid <harun35-2093@diu.edu.bd>
2022-06-17 03:04:57 +0600

    user component added

---
+++ build/static/js/main.1b8a2ccf.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-the-complete-guide"]=this["webpackJsonpreact-the-complete-guide"]||[]).push([[0],{12:function(e,t,c){},14:function(e,t,c){"use strict";c.r(t);c(0);var n=c(3),o=c.n(n),r=(c(12),c(1));var i=function(){return Object(r.jsx)("div",{})};o.a.createRoot(document.getElementById("root")).render(Object(r.jsx)(i,{}))}},[[14,1,2]]]);
+//# sourceMappingURL=main.1b8a2ccf.chunk.js.map
