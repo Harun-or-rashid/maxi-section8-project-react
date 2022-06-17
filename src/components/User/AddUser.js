193802dcbac4660632cf3c2f6932e0f6a26bcbde
Harun-or-rashid <harun35-2093@diu.edu.bd>
2022-06-17 15:51:53 +0600

    changed

--- src/components/User/AddUser.js
+++ src/components/User/AddUser.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from '../UI/Card'
+import classes from './AddUser.module.css'
 const AddUser=(props)=>{
     const submitHandler=(event)=>{
         event.preventDefault();
@@ -9,11 +10,11 @@ const AddUser=(props)=>{
          <Card>
       
      <form onSubmit={submitHandler}>
-     <label for="username">User Name</label>
-        <input id="username" type="text"/>
+     <label className="classes.inputlabel" for="username">User Name</label>
+        <input className={classes.input} id="username" type="text"/>
 
         <label for="age">Age</label>
-        <input id="age" type="number"/>
+        <input className={classes.input} id="age" type="number"/>
 
         <button type="submit">Add User</button>
      </form>
