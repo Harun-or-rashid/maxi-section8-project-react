abb5cd02c3184cfecaa893c9b3ec0152c7c9c9a9
Harun-or-rashid <harun35-2093@diu.edu.bd>
2022-07-20 18:51:22 +0600

    user state added

--- src/components/User/AddUser.js
+++ src/components/User/AddUser.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "../UI/Button";
 import Card from '../UI/Card'
 import classes from './AddUser.module.css'
 const AddUser=(props)=>{
+
+    const [enteredUser,setEnteredUser]=useState('');
     const submitHandler=(event)=>{
         event.preventDefault();
     };
+    const addUserHandler=(event)=>{
+        setEnteredUser(event.target.value);
+    }
 
       return(
          <Card className={classes.input}>
@@ -16,7 +22,7 @@ const AddUser=(props)=>{
         <label for="age">Age</label>
         <input className={classes.input} id="age" type="number"/>
 
-        <button type="submit">Add User</button>
+        <Button type="submit"> Add User</Button> 
      </form>
      
       </Card>
