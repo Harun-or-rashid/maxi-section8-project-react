f5858dd7c96c7856b42b5a5cbc41ccba2b70ee06
Harun-or-rashid <harun35-2093@diu.edu.bd>
2022-07-24 18:56:59 +0600

    add user list done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddUser from './components/User/AddUser';
+import UserList from './components/User/UserList';
 
 
 function App() {
+  const [usersList,setUsersList]=useState([]);
+  const addUserHandler=(uName,uAge)=>{
+    setUsersList((prevUsersList)=>{
+      return[...prevUsersList,{name:uName,age:uAge}];
+    });
+  };
   return (
     <div> 
-      <AddUser/>
+      <AddUser onAddUser={addUserHandler}/>
+      <UserList users={usersList} />
     </div>
   );
 }
